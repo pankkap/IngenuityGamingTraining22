db1c5a0b3d9b80b0275c8b4a96e14e28aa41a424
pankkap <pankkap@gmail.com>
2022-08-22 10:45:44 +0530

    Day-13 Code

--- Day-10 Adv_Functions/4-Scheduling.js
+++ Day-10 Adv_Functions/4-Scheduling.js
@@ -1,16 +1,24 @@
-// setTimeout function
+// Scheduling FUnctions
+// setTimeout allows us to run a function once after the interval of time.
+// setInterval allows us to run a function repeatedly, starting after the interval of time, then repeating continuously at that interval.
 
-// setTimeout(callback, 3000);
+// setTimeout function
+setTimeout(() => {
+  console.log("hello hello ");
+}, 5000);
+
+//setTimeOut with function
+setTimeout(callback, 3000);
 
 function callback() {
   console.log("Hello Friends");
 }
 
-setTimeout(() => {
-  console.log("hello hello ");
-}, 5000);
-
 // setTimeinterval
+setInterval(() => {
+  console.log("Hello Friends");
+}, 2000);
 
+// Stop Interval using clearInterval
 var myInterval = setInterval(callback, 2000);
 clearInterval(myInterval);
